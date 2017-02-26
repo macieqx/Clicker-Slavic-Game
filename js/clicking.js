83618f9a297a2890634b286aa3c6ea7582c1f916
macieqx <mac.bogacz@gmail.com>
2017-02-26 12:49:06 +0100

    Third commit
    Added basic bosses pictures, changing level, when boss is killed, adding
    money, suming damage

--- js/clicking.js
+++ js/clicking.js
@@ -1,11 +1,13 @@
 //Global variables on start
 
-var clickDamage = 2;
+var clickDamage = 1;
+var idleDamage = 0;
 var criticalHitChance = 0;
 var clickCount = 0;
 var money = 0;
 var totalDamage = 0;
 var currentLevel = 0;
+var currentBoss = bosses[0];
 
 //Clicking on monster do something
 
@@ -14,37 +16,58 @@ document.querySelector('.monsters').addEventListener("click", Killing);
 //function onclick
 
 function Killing() {
-	clickCount++;
-	totalDamage += clickDamage;
-	Levelcheck();
-	console.log(clickCount);
-	console.log(totalDamage);
-	console.log(currentLevel);
+    "use strict";
+
+    clickCount++;
+    totalDamage += clickDamage;
+    money += bosses[currentLevel].money;
+    Levelcheck();
+    console.log(clickCount);
+    console.log(totalDamage);
+    console.log(currentLevel);
+    console.log(money);
+    document.getElementById('currentLevelText').innerHTML = "Obecny poziom to " + currentLevel;
 }
 
 //function to check current level
 
 function Levelcheck() {
 
-if (totalDamage < 100) {
-	currentLevel = 1;
+    "use strict";
+
+    if (totalDamage < bosses[0].hp) {
+        currentLevel = 0;
+    }
+    else if (totalDamage < bosses[1].hp) {
+        currentLevel = 1;
+        document.querySelector('.monsters').style.backgroundImage="url('img/boss1.jpg')";
+    }
+    else if (totalDamage < bosses[2].hp) {
+        currentLevel = 2;
+        document.querySelector('.monsters').style.backgroundImage="url('img/boss2.jpg')";
+    }
+    else if (totalDamage < bosses[3].hp) {
+        currentLevel = 3;
+        document.querySelector('.monsters').style.backgroundImage="url('img/boss3.jpg')";
+    }
+    else if (totalDamage < bosses[4].hp) {
+        currentLevel = 4;
+        document.querySelector('.monsters').style.backgroundImage="url('img/boss4.jpg')";
+    }
+    else if (totalDamage < bosses[5].hp) {
+        currentLevel = 5;
+        document.querySelector('.monsters').style.backgroundImage="url('img/boss5.jpg')";
+    }
+    else if (totalDamage < bosses[6].hp) {
+        currentLevel = 6;
+        document.querySelector('.monsters').style.backgroundImage="url('img/boss6.jpg')";
+    }
+    else {
+        currentLevel = 7;
+        document.querySelector('.monsters').style.backgroundImage="url('img/boss7.jpg')";
+    }
 }
-else if (totalDamage <500) {
-	currentLevel = 2;
-}
-else if (totalDamage <1500) {
-	currentLevel = 3;
-}
-else if (totalDamage <3500) {
-	currentLevel = 4;
-}
-else if (totalDamage <6500) {
-	currentLevel = 5;
-}
-else if (totalDamage <10500) {
-	currentLevel = 6;
-}
-else {
-	currentLevel = 7;
-}
-}
+
+function Upgrading(){
+	console.log(clickDamage);
+}; //to be written
--- js/monsters.js
+++ js/monsters.js
@@ -1,3 +1,28 @@
-var monsters;
+var bosses;
 
-monsters = ["xyz", "abc"];
+var Tutorial = {
+    name: "Demo",
+    hp: 10,
+    money: 0.1,
+    type: "without"
+};
+
+var Bagiennik = {
+    name: "Bagiennik",
+    hp: 100,
+    money: 1,
+    type: "water"
+};
+
+var BabaYaga = {
+    name: "Baba Yaga",
+    hp: 350,
+    money: 1,
+    type: "forest"
+};
+
+bosses = [Tutorial, Bagiennik, BabaYaga];
+console.log(bosses[0]);
+console.log(bosses[1]);
+console.log(bosses[0].hp);
+console.log(bosses[1].type);
