c503161b5ee382ddc62a1e6d436d4d47f430ea10
macieqx <mac.bogacz@gmail.com>
2017-03-02 22:19:44 +0100

    8th commit
    Code is now easier, and faster for checking level, instead of if
    condition, we have checker based on current level

--- js/damage.js
+++ js/damage.js
@@ -38,88 +38,23 @@ function Levelcheck() {
 
     console.log(totalDamage);
 
-    if (totalDamage < bosses[0].totalhp) {
+    if (totalDamage < bosses[currentLevel].totalhp) {
         currentLevel = 0;
     }
-    else if (totalDamage < bosses[1].totalhp) {
-        currentLevel = 1;
-        document.querySelector('.monsters').style.backgroundImage = "url('img/boss1.jpg')";
-    }
-    else if (totalDamage < bosses[2].totalhp) {
-        currentLevel = 2;
-        document.querySelector('.monsters').style.backgroundImage = "url('img/boss2.jpg')";
-    }
-    else if (totalDamage < bosses[3].totalhp) {
-        currentLevel = 3;
-        document.querySelector('.monsters').style.backgroundImage = "url('img/boss3.jpg')";
-    }
-    else if (totalDamage < bosses[4].totalhp) {
-        currentLevel = 4;
-        document.querySelector('.monsters').style.backgroundImage = "url('img/boss4.jpg')";
-    }
-    else if (totalDamage < bosses[5].totalhp) {
-        currentLevel = 5;
-        document.querySelector('.monsters').style.backgroundImage = "url('img/boss5.jpg')";
-    }
-    else if (totalDamage < bosses[6].totalhp) {
-        currentLevel = 6;
-        document.querySelector('.monsters').style.backgroundImage = "url('img/boss6.jpg')";
-    }
-    else if (totalDamage < bosses[7].totalhp) {
-        currentLevel = 7;
-        document.querySelector('.monsters').style.backgroundImage = "url('img/boss7.jpg')";
-    }
-    else if (totalDamage < bosses[8].totalhp) {
-        currentLevel = 8;
-        document.querySelector('.monsters').style.backgroundImage = "url('img/boss8.jpg')";
-    }
-    else if (totalDamage < bosses[9].totalhp) {
-        currentLevel = 9;
-        document.querySelector('.monsters').style.backgroundImage = "url('img/boss9.jpg')";
-    }
-    else if (totalDamage < bosses[10].totalhp) {
-        currentLevel = 10;
-        document.querySelector('.monsters').style.backgroundImage = "url('img/boss10.jpg')";
-    }
-    else if (totalDamage < bosses[11].totalhp) {
-        currentLevel = 11;
-        document.querySelector('.monsters').style.backgroundImage = "url('img/boss11.jpg')";
-    }
-    else if (totalDamage < bosses[12].totalhp) {
-        currentLevel = 12;
-        document.querySelector('.monsters').style.backgroundImage = "url('img/boss12.jpg')";
-    }
-    else if (totalDamage < bosses[13].totalhp) {
-        currentLevel = 13;
-        document.querySelector('.monsters').style.backgroundImage = "url('img/boss13.jpg')";
-    }
-    else if (totalDamage < bosses[14].totalhp) {
-        currentLevel = 14;
-        document.querySelector('.monsters').style.backgroundImage = "url('img/boss14.jpg')";
-    }
-    else if (totalDamage < bosses[15].totalhp) {
-        currentLevel = 15;
-        document.querySelector('.monsters').style.backgroundImage = "url('img/boss15.jpg')";
-    }
-    else if (totalDamage < bosses[16].totalhp) {
-        currentLevel = 16;
-        document.querySelector('.monsters').style.backgroundImage = "url('img/boss16.jpg')";
-    }
-    else if (totalDamage < bosses[17].totalhp) {
-        currentLevel = 17;
-        document.querySelector('.monsters').style.backgroundImage = "url('img/boss17.jpg')";
-    }
     else {
-        return false;
+        currentLevel++;
+        document.querySelector('.monsters').style.backgroundImage = "url('img/boss" + currentLevel + ".jpg')";
     }
 }
-
+   
 function IdleClick() {
+
      if (idleDamage < currentHP) {  //If you can't kill do damage
         totalDamage += idleDamage;
         currentHP -= idleDamage;
         console.log("opcja1");
     }
+
     else {  //If you can kill deal damage equal to currentHP and change level
         totalDamage += currentHP;
         currentHP = 0;
